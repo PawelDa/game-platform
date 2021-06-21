ff36598cd3ae2876888358df3f69d0c326624bbf
Pawel Dabkowski <paweldabkowski01@gmail.com>
2021-06-21 22:03:49 +0100

    users route test

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-// @route   GET api/users
-// @desc    Test route
-// @acces   Public
+// @route           GET api/users
+// @description     Test route
+// @acces           Public
 router.get('/', (req, res) => res.send('User route'));
 
 module.exports = router;

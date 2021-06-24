3b4e9779ad82511a7a0c8e314f2322b53bbc9ff9
Pawel Dabkowski <paweldabkowski01@gmail.com>
2021-06-24 23:23:34 +0100

    added express validator to validate inputs when creating user

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,12 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const { body, validationResult } = require('express-validator');
 
 // @route           POST api/users
 // @description     Register user
 // @acces           Public
-router.post('/', (req, res) => {
-    console.log(req.body);
+router.post(
+  '/',
+  // user validation
+  body('name', 'Name is required').not().notEmpty(),
+  // email validation
+  body('email', 'Please provide a valid email address').isEmail(),
+  // password validation
+  body('password', 'Please enter a password with 6 or more characters').isLength({ min: 6 }),
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
     res.send('User route');
-});
+  }
+);
 
 module.exports = router;

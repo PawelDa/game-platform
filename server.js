c0f78e3dc40695f1418720fe0ccf0529913615e2
Pawel Dabkowski <paweldabkowski01@gmail.com>
2021-06-24 22:43:12 +0100

    tables route added

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ app.get('/', (req, res) => res.send('API running'));
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/tables', require('./routes/api/tables'));
 
 const PORT = process.env.PORT || 5000;
 

7aa0d76401a6a56666a1a999a37e1e2169779c3d
Pawel Dabkowski <paweldabkowski01@gmail.com>
2021-06-25 22:17:20 +0100

    imort user model

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const { body, validationResult } = require('express-validator');
 
+const User = require('../../models/User');
+
 // @route           POST api/users
 // @description     Register user
 // @acces           Public
@@ -18,6 +20,8 @@ router.post(
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
+    
+
 
     res.send('User route');
   }

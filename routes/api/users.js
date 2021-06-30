c716e6bc1ec22e30ec7baf144f3e43237baa5453
Pawel Dabkowski <paweldabkowski01@gmail.com>
2021-06-30 20:03:32 +0100

    implemented action to be able registered user, get avatar, hash password and test it with postman

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
 const { body, validationResult } = require('express-validator');
-
+// Getting User model
 const User = require('../../models/User');
 
 // @route           POST api/users
@@ -15,15 +17,49 @@ router.post(
   body('email', 'Please provide a valid email address').isEmail(),
   // password validation
   body('password', 'Please enter a password with 6 or more characters').isLength({ min: 6 }),
-  (req, res, next) => {
+  async (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
     
+    const { name, email, password } = req.body;
 
+    try {
+      let user = await User.findOne({ email });
+      //if User exists
+      if(user) {
+        return res.status(400).json({ errors: [{ msg: 'User already exists'}] });
+      }
 
-    res.send('User route');
+      const avatar = gravatar.url(email, {
+        s: '200',
+        r: 'pg',
+        d: 'mm'
+      });
+
+      user = new User({
+        name,
+        email,
+        avatar,
+        password
+      });
+
+      //Encypt password
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt);
+
+      // saving user
+      await user.save();
+
+      // TODO return jsonwebtoken
+
+      res.send('User registered');
+    } catch(err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
+    }
   }
 );
 

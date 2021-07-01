f8075d32d1637a3aa314d590ed64eafc8bf37ef1
Pawel Dabkowski <paweldabkowski01@gmail.com>
2021-07-01 18:27:30 +0100

    added middleware to secure route

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
 
 // @route           GET api/auth
 // @description     Test route
 // @acces           Public
-router.get('/', (req, res) => res.send('Auth route'));
+router.get('/', auth, (req, res, next) => res.send('Auth route'));
 
 module.exports = router;

fab109bb18ba3324064c40205e4e06b1154536c3
Pawel Dabkowski <paweldabkowski01@gmail.com>
2021-07-06 20:43:23 +0100

    require user model to authentication route

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,9 +2,17 @@ const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth');
 
+const User = require('../../models/User');
+
 // @route           GET api/auth
 // @description     Test route
 // @acces           Public
-router.get('/', auth, (req, res, next) => res.send('Auth route'));
+router.get('/', auth, async (req, res, next) => {
+  try {
+    const user = await User
+  } catch(err) {
+
+  }
+});
 
 module.exports = router;

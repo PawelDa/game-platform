3044093f3eeb41a364e600d83954eb19796e36da
Pawel Dabkowski <paweldabkowski01@gmail.com>
2021-07-07 22:34:58 +0100

    create get request for user profile

--- server.js
+++ server.js
@@ -17,7 +17,6 @@ app.get('/', (req, res, next) => {
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
-app.use('/api/tables', require('./routes/api/tables'));
 
 const PORT = process.env.PORT || 5000;
 

e373e42867de7019afa5d64b476ac2a942002bd5
Pawel Dabkowski <paweldabkowski01@gmail.com>
2021-07-07 22:19:34 +0100

    delete tables router

--- routes/api/tables.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-// @route           GET api/tables
-// @description     Test route
-// @acces           Public
-router.get('/', (req, res) => res.send('Table route'));
-
-module.exports = router;

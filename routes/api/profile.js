567d782bdaa65ef0d17112d8f03899b076cbccac
Pawel Dabkowski <paweldabkowski01@gmail.com>
2021-07-07 22:39:27 +0100

    required express-validator

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth');
+const { body, validationResult } = require('express-validator');
 
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
@@ -14,7 +15,7 @@ router.get('/me', auth, async (req, res) => {
       'user',
       ['name', 'avatar']
     );
-    
+
     if(!profile) {
       res.status(400).json({ msg: 'There is no profile for this user' });
     }

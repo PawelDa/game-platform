3dfae2a1c6028299c224fbbd7e2d6690f19aa391
Pawel Dabkowski <paweldabkowski01@gmail.com>
2021-07-07 22:14:38 +0100

    created Profile model schema

---
+++ models/Profile.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+
+const ProfileSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user'
+  },
+  special: {
+    type: Boolean,
+    default: false
+  },
+  bio: {
+    type: String
+  },
+  games: {
+    winner: {
+      type: Number,
+      default: 0
+    },
+    secondPlace: {
+      type: Number,
+      default: 0
+    },
+    thirdPlace: {
+      type: Number,
+      default: 0
+    },
+    forthPlace: {
+      type: Number,
+      default: 0
+    },
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = Profile = mongoose.model('profile', ProfileSchema);

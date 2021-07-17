324a8badf9dad331195680bcc402f86eea4c891e
Pawel Dabkowski <paweldabkowski01@gmail.com>
2021-07-17 16:18:17 +0100

    Table model schema

--- models/Table.js
+++ models/Table.js
@@ -1,10 +1,26 @@
 const mongoose = require('mongoose');
 
 const TableSchema = new mongoose.Schema({
-  user: {
+  admin: {
     type: Schema.Types.ObjectId,
     ref: 'users'
-  }
+  },
+  secondPlayer: {
+    type: Schema.Types.ObjectId,
+    ref: 'users'
+  },
+  thirdPlayer: {
+    type: Schema.Types.ObjectId,
+    ref: 'users'
+  },
+  numberOfPlayers: {
+    type: Number,
+    default: 3
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  },
   // TODO add another 2 players to table schema which were not admins
 });
 

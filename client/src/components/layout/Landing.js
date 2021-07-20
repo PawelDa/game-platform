1a3f71db9d2fb785a7c0324d68f0ce0a9473cadb
Pawel Dabkowski <paweldabkowski01@gmail.com>
2021-07-20 11:38:37 +0100

    build landing component with bootstrap

---
+++ client/src/components/layout/Landing.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Landing = () => {
+  return (
+    <div class="card text-center">
+      <div class="card-body">
+        <h5 class="card-title">Play 1000</h5>
+        <p class="card-text">Create account to enjoy playing.</p>
+        <a href="#" class="btn btn-primary">Sign Up</a>
+        <a href="#" class="btn btn-primary">Login</a>
+      </div>
+    </div>
+  )
+}
+
+export default Landing;

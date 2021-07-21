1943fb6963882ed1f623c0218713dc260d9dd376
Pawel Dabkowski <paweldabkowski01@gmail.com>
2021-07-21 16:02:25 +0100

    added Login and Register routes to landing page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,13 +2,21 @@ import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar.jsx';
 import Landing from './components/layout/Landing.jsx';
+import Register from './components/auth/Register.jsx';
+import Login from './components/auth/Login.jsx';
 
-function App() {
+const App = () => {
   return (
     <Router>
       <Fragment>
         <Navbar />
         <Route exact path='/' component = {Landing} />
+        <section className='container'>
+          <Switch>
+            <Route exact path='/register' component={Register} />
+            <Route exact path='/login' component={Login} />
+          </Switch>
+        </section>
       </Fragment>
     </Router>
   );

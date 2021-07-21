fef924dfbf9e2422716fa3709c2ae9e954f4e501
Pawel Dabkowski <paweldabkowski01@gmail.com>
2021-07-21 16:07:53 +0100

    added link to login and register to navbar

--- client/src/components/layout/Navbar.jsx
+++ client/src/components/layout/Navbar.jsx
@@ -1,20 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container-fluid">
-        <a className="navbar-brand" href="#">1000 game platfrom</a>
+        <Link className="navbar-brand" to='/'>1000 game platfrom</Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarText">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <a className="nav-link" href="#">Register</a>
+              <Link className="nav-link" to='/register'>Register</Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">Login</a>
+              <Link className="nav-link" to='/login'>Login</Link>
             </li>
           </ul>
         </div>

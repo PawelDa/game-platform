43e166a424671f917c1c5c2984a43355eab7b604
Pawel Dabkowski <paweldabkowski01@gmail.com>
2021-07-22 18:00:03 +0100

    changed input fields in register form

--- client/src/components/auth/Register.jsx
+++ client/src/components/auth/Register.jsx
@@ -1,21 +1,31 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 
 const Register = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    password2: ''
+  });
+
   return (
     <Fragment>
       <form>
         <div className="mb-3">
+          <label for="exampleInputEmail1" className="form-label">User name</label>
+          <input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+        </div>
+        <div className="mb-3">
           <label for="exampleInputEmail1" className="form-label">Email address</label>
           <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-          <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
         </div>
         <div className="mb-3">
           <label for="exampleInputPassword1" className="form-label">Password</label>
           <input type="password" className="form-control" id="exampleInputPassword1" />
         </div>
-        <div className="mb-3 form-check">
-          <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-          <label className="form-check-label" for="exampleCheck1">Check me out</label>
+        <div className="mb-3">
+          <label for="exampleInputPassword1" className="form-label">Confirm Password</label>
+          <input type="password" className="form-control" id="exampleInputPassword1" />
         </div>
         <button type="submit" className="btn btn-primary">Submit</button>
       </form>

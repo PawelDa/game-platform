d0953fbd8cd5bf7202faa6efaf3a1ca6d6b31b2f
Pawel Dabkowski <paweldabkowski01@gmail.com>
2021-07-23 15:30:35 +0100

    added console error to try-catch statment when creating user

--- client/src/components/auth/Register.jsx
+++ client/src/components/auth/Register.jsx
@@ -19,24 +19,25 @@ const Register = () => {
       // TODO add allert later for now console.log
       console.log('Passwords do not match');
     } else {
-      const newUser = { name, email, password },
+      const newUser = { name, email, password };
       // Creating new User
+
       try {
         const config = {
           headers: {
             'Content-Type': 'application/json'
           }
-        }
+        };
 
         const body = JSON.stringify(newUser)
 
-        const res = await axios.post('/api/users', body, cofig);
+        const res = await axios.post('/api/users', body, config);
         console.log(res.data)
       } catch (error) {
-
+        console.error(error.response.data);
       }
     }
-  }
+  };
 
   return (
     <Fragment>

b955be2d9d68c473b14ca581070fcc4ab4ffecff
Pawel Dabkowski <paweldabkowski01@gmail.com>
2021-07-23 15:47:12 +0100

    added login form

--- client/src/components/auth/Login.jsx
+++ client/src/components/auth/Login.jsx
@@ -1,10 +1,50 @@
-import React from 'react';
+import React, { Fragment, useState } from 'react';
 
 const Login = () => {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
+
+  const { email, password } = formData;
+
+  const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = async e => {
+    e.preventDefault();
+    console.log('User loged in!')
+  };
+
   return (
-    <div>
-      Login
-    </div>
+    <Fragment>
+      <form onSubmit={e => onSubmit(e)}>
+        <div className="mb-3">
+          <input
+            placeholder="Email address"
+            name='email'
+            type="email"
+            className="form-control"
+            id="exampleInputEmail1"
+            value={email}
+            onChange={e => onChange(e)}
+            required
+          />
+        </div>
+        <div className="mb-3">
+          <input
+            placeholder="Password"
+            name='password'
+            type="password"
+            className="form-control"
+            id="exampleInputPassword1"
+            value={password}
+            onChange={e => onChange(e)}
+            required
+          />
+        </div>
+        <button type="submit" className="btn btn-primary">Login</button>
+      </form>
+    </Fragment>
   )
 };
 

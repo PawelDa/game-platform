8ec6a032c124e662d4027c82de0a6bec22dacc47
Pawel Dabkowski <paweldabkowski01@gmail.com>
2021-07-26 17:11:25 +0100

    destructured props in Register component

--- client/src/components/auth/Register.jsx
+++ client/src/components/auth/Register.jsx
@@ -2,7 +2,7 @@ import React, { Fragment, useState } from 'react';
 import { connect } from 'react-redux';
 import { setAlert } from '../../actions/alert';
 
-const Register = (props) => {
+const Register = ({ setAlert }) => {
   const [formData, setFormData] = useState({
     name: '',
     email: '',
@@ -17,7 +17,7 @@ const Register = (props) => {
   const onSubmit = async e => {
     e.preventDefault();
     if (password !== password2) {
-      props.setAlert('Passwords do not match', 'danger');
+      setAlert('Passwords do not match', 'danger');
     } else {
       console.log('User registered!')
     }

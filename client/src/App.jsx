ec1fccae9b72e78079a1e81a8f1bd5d82ad48d4e
Pawel Dabkowski <paweldabkowski01@gmail.com>
2021-07-27 19:38:09 +0100

    Alert map state to props

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from './components/layout/Navbar.jsx';
 import Landing from './components/layout/Landing.jsx';
 import Register from './components/auth/Register.jsx';
 import Login from './components/auth/Login.jsx';
+import Alert from './components/layout/Alert';
 // Redux
 import { Provider } from 'react-redux';
 import store from './store';
@@ -16,6 +17,7 @@ const App = () => {
           <Navbar />
           <Route exact path='/' component = {Landing} />
           <section className='container'>
+            <Alert />
             <Switch>
               <Route exact path='/register' component={Register} />
               <Route exact path='/login' component={Login} />

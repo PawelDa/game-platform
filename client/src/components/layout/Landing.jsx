14f1a537475ba7bff04af68b1fbdf645d2ce0275
Pawel Dabkowski <paweldabkowski01@gmail.com>
2021-07-27 19:01:43 +0100

    reorganizing scss files structure

--- client/src/components/layout/Landing.jsx
+++ client/src/components/layout/Landing.jsx
@@ -3,12 +3,14 @@ import { Link } from 'react-router-dom';
 
 const Landing = () => {
   return (
-    <div class="card text-center">
-      <div class="card-body">
-        <h5 class="card-title">1000 card game</h5>
-        <p class="card-text">Create an account or login to enjoy the game!</p>
-        <Link to="/register" class="btn btn-primary">Sign Up</Link>
-        <Link to="/login" class="btn btn-primary">Login</Link>
+    <div className="card text-center">
+      <div className="card-body">
+        <h5 className="card-title">1000 card game</h5>
+        <p className="card-text">Create an account or login to enjoy the game!</p>
+        <div className="d-grid gap-2 d-md-block">
+          <Link to="/register" className="btn btn-primary me-md-2">Sign Up</Link>
+          <Link to="/login" className="btn btn-primary">Login</Link>
+        </div>
       </div>
     </div>
   )

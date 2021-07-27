f5c7f8738e4f700d95799c1e67e5ad26e77b6497
Pawel Dabkowski <paweldabkowski01@gmail.com>
2021-07-27 19:05:59 +0100

    landing page buttons width changed

--- client/src/components/layout/Landing.jsx
+++ client/src/components/layout/Landing.jsx
@@ -8,8 +8,8 @@ const Landing = () => {
         <h5 className="card-title">1000 card game</h5>
         <p className="card-text">Create an account or login to enjoy the game!</p>
         <div className="d-grid gap-2 d-md-block">
-          <Link to="/register" className="btn btn-primary me-md-2">Sign Up</Link>
-          <Link to="/login" className="btn btn-primary">Login</Link>
+          <Link to="/register" className="btn btn-primary me-md-2 landing-button">Sign Up</Link>
+          <Link to="/login" className="btn btn-primary landing-button">Login</Link>
         </div>
       </div>
     </div>

a1efcd26e729522465c9397e858f6f0d98e9c6dd
Pawel Dabkowski <paweldabkowski01@gmail.com>
2021-07-27 18:34:17 +0100

    changed header and paragraf on landing page

--- client/src/components/layout/Landing.jsx
+++ client/src/components/layout/Landing.jsx
@@ -5,8 +5,8 @@ const Landing = () => {
   return (
     <div class="card text-center">
       <div class="card-body">
-        <h5 class="card-title">Play 1000</h5>
-        <p class="card-text">Create account to enjoy playing.</p>
+        <h5 class="card-title">1000 card game</h5>
+        <p class="card-text">Create an account or login to enjoy the game!</p>
         <Link to="/register" class="btn btn-primary">Sign Up</Link>
         <Link to="/login" class="btn btn-primary">Login</Link>
       </div>

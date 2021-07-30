a6fd9f5729b6d52ca378d6eadffcfe380a9995d4
Pawel Dabkowski <paweldabkowski01@gmail.com>
2021-07-30 07:32:43 +0100

    auth reducer with 2 cases: fail and success register actions

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -1,5 +1,6 @@
 import { REGISTER_FAIL, REGISTER_SUCCESS } from "../actions/types";
 
+// this is state befor user will login and get access to private routes
 const initialState = {
   token: localStorage.getItem('token'),
   // when token will be assigned then isAuthnticated variable will be True
@@ -9,3 +10,30 @@ const initialState = {
   // then User will be assign
   user: null
 };
+
+const authReducer = (state = initialState, action) => {
+  const { type, payload } = action;
+
+  switch (type) {
+    case REGISTER_SUCCESS:
+      localStorage.setItem('token', payload.token);
+      return {
+        ...state,
+        ...payload,
+        isAuthenticated: true,
+        loading: false
+      }
+    case REGISTER_FAIL:
+      localStorage.removeItem('token');
+      return {
+        ...state,
+        token: null,
+        isAuthenticated: false,
+        loading: false
+      }
+    default:
+      return state;
+  }
+};
+
+export default authReducer;

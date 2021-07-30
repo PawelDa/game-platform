e106a9707014f8aa299ee1c156891ab10ed3abef
Pawel Dabkowski <paweldabkowski01@gmail.com>
2021-07-30 08:06:29 +0100

    added set alert to register action

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { setAlert } from './alert';
 import { REGISTER_SUCCESS, REGISTER_FAIL } from './types';
 
 // User registration action
@@ -18,7 +19,13 @@ export const register = ({ name, email, password }) => async dispatch => {
       type: REGISTER_SUCCESS,
       payload: res.data
     });
-  } catch (error) {
+  } catch (err) {
+    const errors = err.response.data.errors;
+
+    if (errors) {
+      errors.forEach(error => dispatch(setAlert(error.msg, 'danger')))
+    }
+
     dispatch({
       type: REGISTER_FAIL
     });

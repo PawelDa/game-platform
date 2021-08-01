d5b56a2c7e7c480e736419fa5be66ff7573445ae
Pawel Dabkowski <paweldabkowski01@gmail.com>
2021-08-01 19:31:41 +0100

    connect login on submit action in component

--- client/src/components/auth/Login.jsx
+++ client/src/components/auth/Login.jsx
@@ -1,6 +1,9 @@
 import React, { Fragment, useState } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { login } from '../../actions/auth';
 
-const Login = () => {
+const Login = ({ login }) => {
   const [formData, setFormData] = useState({
     email: '',
     password: '',
@@ -12,7 +15,7 @@ const Login = () => {
 
   const onSubmit = async e => {
     e.preventDefault();
-    console.log('User loged in!')
+    login(email, password);
   };
 
   return (
@@ -48,4 +51,8 @@ const Login = () => {
   )
 };
 
-export default Login;
+Login.propTypes = {
+  login: PropTypes.func.isRequired
+}
+
+export default connect(null, { login })(Login);

bfd430e3d530f5c767917d128ae9e7a3e50c1777
Pawel Dabkowski <paweldabkowski01@gmail.com>
2021-08-02 19:23:14 +0100

    added action to redirect user to dashboard after login

--- client/src/components/auth/Login.jsx
+++ client/src/components/auth/Login.jsx
@@ -1,9 +1,10 @@
 import React, { Fragment, useState } from 'react';
+import { Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { login } from '../../actions/auth';
 
-const Login = ({ login }) => {
+const Login = ({ login, isAuthenticated }) => {
   const [formData, setFormData] = useState({
     email: '',
     password: '',
@@ -17,6 +18,11 @@ const Login = ({ login }) => {
     e.preventDefault();
     login(email, password);
   };
+
+  // Redirecting if loging in successfull
+  if (isAuthenticated) {
+    return <Redirect to='/dashboard' />;
+  }
 
   return (
     <Fragment>
@@ -52,7 +58,12 @@ const Login = ({ login }) => {
 };
 
 Login.propTypes = {
-  login: PropTypes.func.isRequired
-}
+  login: PropTypes.func.isRequired,
+  isAuthenticated: PropTypes.bool
+};
 
-export default connect(null, { login })(Login);
+const mapStateToProps = state => ({
+  isAuthenticated: state.auth.isAuthenticated
+});
+
+export default connect(mapStateToProps, { login })(Login);

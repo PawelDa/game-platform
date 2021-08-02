199481e98776fc333cf78e7789a5b31f11487c34
Pawel Dabkowski <paweldabkowski01@gmail.com>
2021-08-02 19:51:16 +0100

    changes in navbar buttons for loged in and new users

--- client/src/components/layout/Navbar.jsx
+++ client/src/components/layout/Navbar.jsx
@@ -1,7 +1,35 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { logout } from '../../actions/auth';
 
-const Navbar = () => {
+const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
+  const authLinks = (
+    <div className="collapse navbar-collapse" id="navbarText">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <a onClick={logout} className="nav-link" href='#!'>
+          <i className="fas fa-sign-out-alt"></i>{' '}
+          Logout</a>
+        </li>
+      </ul>
+    </div>
+  );
+
+  const questinks = (
+    <div className="collapse navbar-collapse" id="navbarText">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <Link className="nav-link" to='/register'>Register</Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" to='/login'>Login</Link>
+        </li>
+      </ul>
+    </div>
+  );
+
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container-fluid">
@@ -9,19 +37,19 @@ const Navbar = () => {
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse" id="navbarText">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <Link className="nav-link" to='/register'>Register</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to='/login'>Login</Link>
-            </li>
-          </ul>
-        </div>
+        { !loading && (<Fragment>{ isAuthenticated ? authLinks : questinks }</Fragment>) }
       </div>
     </nav>
   )
 };
 
-export default Navbar;
+Navbar.propTypes = {
+  logout: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  auth: state.auth
+});
+
+export default connect(mapStateToProps, { logout })(Navbar);

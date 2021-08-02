fa0e317e7140f0c6a16306bb6e945f59dd66d4bc
Pawel Dabkowski <paweldabkowski01@gmail.com>
2021-08-02 19:27:57 +0100

    logout case added to recuder

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -4,7 +4,8 @@ import {
   USER_LOADED,
   AUTH_ERROR,
   LOGIN_SUCCESS,
-  LOGIN_FAIL
+  LOGIN_FAIL,
+  LOGOUT
 } from "../actions/types";
 
 // this is state befor user will login and get access to private routes
@@ -41,6 +42,7 @@ const authReducer = (state = initialState, action) => {
     case REGISTER_FAIL:
     case AUTH_ERROR:
     case LOGIN_FAIL:
+    case LOGOUT:
       localStorage.removeItem('token');
       return {
         ...state,

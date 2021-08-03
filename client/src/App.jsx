5c55589f193082fe6f3526b41c7e56d27d3f4cb3
Pawel Dabkowski <paweldabkowski01@gmail.com>
2021-08-03 22:11:59 +0100

    added dashboard component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import Landing from './components/layout/Landing.jsx';
 import Register from './components/auth/Register.jsx';
 import Login from './components/auth/Login.jsx';
 import Alert from './components/layout/Alert';
+import Dashboard from './components/dashboard/Dashboard';
 // Redux
 import { Provider } from 'react-redux';
 import store from './store';
@@ -31,12 +32,13 @@ const App = () => {
             <Switch>
               <Route exact path='/register' component={Register} />
               <Route exact path='/login' component={Login} />
+              <Route exact path='/dashboard' component={Dashboard} />
             </Switch>
           </section>
         </Fragment>
       </Router>
     </Provider>
   );
-}
+};
 
 export default App;
---
+++ client/src/components/dashboard/Dashboard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Dashboard = () => {
+  return (
+    <div>Hello</div>
+  )
+};
+
+Dashboard.propTypes = {};
+
+export default Dashboard;

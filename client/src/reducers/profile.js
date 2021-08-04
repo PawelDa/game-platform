e6392c2fb2705566143f7011db98fb5c681bcd91
Pawel Dabkowski <paweldabkowski01@gmail.com>
2021-08-04 05:00:51 +0100

    added profile actions

---
+++ client/src/reducers/profile.js
@@ -0,0 +1,16 @@
+const initialState = {
+  profile: null,
+  profiles: [],
+  loading: true,
+  error: {}
+};
+
+const profileReducer = (state = initialState, action) => {
+  const { type, payload } = action;
+
+  switch (type) {
+      
+  }
+};
+
+export default profileReducer;

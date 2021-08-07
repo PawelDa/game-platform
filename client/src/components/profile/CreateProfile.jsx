c1a7f573e5ebd4de5afd9f5a738071f42145b26d
Pawel Dabkowski <paweldabkowski01@gmail.com>
2021-08-07 19:43:50 +0100

    create profile form component

---
+++ client/src/components/profile/CreateProfile.jsx
@@ -0,0 +1,14 @@
+import React = from 'react';
+import PropTypes from 'prop-types';
+
+const CreateProfile = () => {
+  return (
+    <div></div>
+  )
+};
+
+CreateProfile.propTypes = {
+
+};
+
+export default CreateProfile;

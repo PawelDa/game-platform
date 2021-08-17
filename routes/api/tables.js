50c06671328dd6737b23f72cdc069a2ffbadc670
Pawel Dabkowski <paweldabkowski01@gmail.com>
2021-08-17 22:23:32 +0100

    table schema

---
+++ routes/api/tables.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
+
+// @route           POST api/table
+// @description     Create or update table
+// @acces           Private
+
+module.exports = router;

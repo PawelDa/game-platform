e5b17ac034f76db3893e4c993c9372d748018cde
Pawel Dabkowski <paweldabkowski01@gmail.com>
2021-08-18 14:50:15 +0100

    get all Tables action

--- client/src/actions/table.js
+++ client/src/actions/table.js
@@ -8,8 +8,16 @@ import {
 // Get tables
 export const getTables = () => async dispatch => {
   try {
+    const res = await axios.get('api/tables');
 
-  } catch (error) {
-    
+    dispatch({
+      type: GET_TABLES,
+      payload: res.data
+    });
+  } catch (err) {
+    dispatch({
+      type: TABLE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
   }
 };

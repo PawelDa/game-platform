531f3f83c7b1db495f1ef19954e99d0de07678b7
Pawel Dabkowski <paweldabkowski01@gmail.com>
2021-08-18 14:01:38 +0100

    table actions

---
+++ client/src/actions/table.js
@@ -0,0 +1,15 @@
+import axios form 'axios'
+import { setAlert } from "./alert";
+import {
+  GET_TABLES,
+  TABLE_ERROR
+} from './types';
+
+// Get tables
+export const getTables = () => async dispatch => {
+  try {
+
+  } catch (error) {
+    
+  }
+};

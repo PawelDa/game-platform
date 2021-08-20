3cccc1368e6194fc79355449f8e1fe820de36143
Pawel Dabkowski <paweldabkowski01@gmail.com>
2021-08-20 14:55:42 +0100

    post table route

--- routes/api/tables.js
+++ routes/api/tables.js
@@ -1,13 +1,34 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
 
 const Table = require('../../models/Table');
-// const User = require('../../models/User');
+const User = require('../../models/User');
+const Profile = require('..//../models/Profile');
+
+// @route           POST api/tables
+// @description     Create a table
+// @aacces          Private
+router.post('/', auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+  
+    const table = new Table({
+      user: user.id
+    });
+    await table.save();
+
+    res.json(table);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error')
+  }
+});
 
 // @route           GET api/tables
 // @description     Get all tables
-// @aacces           Private
-router.get('/', auth, async (req, res) =>{
+// @aacces          Private
+router.get('/', auth, async (req, res) => {
   try {
     const tables = await Table.find().sort({ date: -1 });
     res.json(tables);

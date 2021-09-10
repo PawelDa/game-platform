698ceb0e997c17f7bee3417c796ecb299fe2cbaa
Pawel Dabkowski <paweldabkowski01@gmail.com>
2021-09-10 13:53:16 +0100

    changed token for env variable

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,22 +1,22 @@
 const jwt = require('jsonwebtoken');
-const config = require('config');
+require('dotenv').config();
 
 module.exports = function(req, res, next) {
-  // Get the toke from header
+  // Get the token from header
   const token = req.header('x-auth-token');
 
   // Check if there is no token
   if(!token) {
-    return res.status(401).json({ msg: 'No token, authorization denied' });
+    return res.status(401).json({ msg: 'No token' });
   }
 
   // Verify token
   try {
-    const decoded = jwt.verify(token, config.get('jwtToken'));
+    const decoded = jwt.verify(token, process.env.JWT);
 
     req.user = decoded.user;
     next();
   } catch(err) {
-    res.status(401).json({ msg: 'Token is not valid' });
+    res.status(401).json({ msg: 'Token not valid' });
   }
 };

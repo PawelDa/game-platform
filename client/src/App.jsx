5e0a3b869917e161a99e694c746674ebef5a61cb
Pawel Dabkowski <paweldabkowski01@gmail.com>
2021-09-12 19:38:19 +0100

    reorginize assets directory with stylesheets files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,51 +1,11 @@
-import React, { Fragment, useEffect } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Navbar from './components/layout/Navbar.jsx';
-import Landing from './components/layout/Landing.jsx';
-import Register from './components/auth/Register.jsx';
-import Login from './components/auth/Login.jsx';
-import Alert from './components/layout/Alert';
-import Dashboard from './components/dashboard/Dashboard';
-import PrivateRoute from './components/routing/PrivateRoute';
-import CreateProfile from './components/profile/CreateProfile';
-import EditProfile from './components/profile/EditProfile';
-import Tables from './components/tables/Tables.jsx';
-// Redux
-import { Provider } from 'react-redux';
-import store from './store';
-import { loadUser } from './actions/auth';
-import setAuthToken from './utils/setAuthToken';
+import React from 'react';
 
-if (localStorage.token) {
-  setAuthToken(localStorage.token);
-}
+import './assets/stylesheets/application.scss';
 
-const App = () => {
-  useEffect(() => {
-    store.dispatch(loadUser());
-  }, []);
-
-  return (
-    <Provider store={store}>
-      <Router>
-        <Fragment>
-          <Navbar />
-          <Route exact path='/' component = {Landing} />
-          <section className='container'>
-            <Alert />
-            <Switch>
-              <Route exact path='/register' component={Register} />
-              <Route exact path='/login' component={Login} />
-              <PrivateRoute exact path='/dashboard' component={Dashboard} />
-              <PrivateRoute exact path='/create-profile' component={CreateProfile} />
-              <PrivateRoute exact path='/edit-profile' component={EditProfile} />
-              <PrivateRoute exact path='/tables' component={Tables} />
-            </Switch>
-          </section>
-        </Fragment>
-      </Router>
-    </Provider>
-  );
-};
+const App = () => (
+  <div>
+    HELLO
+  </div>
+);
 
 export default App;

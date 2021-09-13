f306c55d46ec0728fe4a60e16da07205c138b3a4
Pawel Dabkowski <paweldabkowski01@gmail.com>
2021-09-13 13:30:07 +0100

    navbar changes

--- client/src/components/layout/Navbar.jsx
+++ client/src/components/layout/Navbar.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 
+import '../../assets/stylesheets/components/layout/Navbar.scss';
+
 const Navbar = () => {
   return (
     <nav className="navbar bg-dark">
       <h1>
-        <a href="index.html"><i className="fas fa-code"></i> DevConnector</a>
+        <a href="index.html"><i className="fas fa-code"></i>EngineersWorld</a>
       </h1>
       <ul>
-        <li><a href="profiles.html">Developers</a></li>
+        <li><a href="profiles.html">Engineers</a></li>
         <li><a href="register.html">Register</a></li>
         <li><a href="login.html">Login</a></li>
       </ul>

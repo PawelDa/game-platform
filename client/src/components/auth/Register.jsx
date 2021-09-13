46f92269f45e6a09d6fed6a2a53bfcebccf6fbe5
Pawel Dabkowski <paweldabkowski01@gmail.com>
2021-09-13 17:24:47 +0100

    register form with onSubmit action forming object formData to create new user

--- client/src/components/auth/Register.jsx
+++ client/src/components/auth/Register.jsx
@@ -1,10 +1,80 @@
-import React from 'react'
+import React, { Fragment, useState } from 'react'
 
 export const Register = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    passwordConfirm: ''
+  });
+
+  const { name, email, password, passwordConfirm } = formData;
+
+  const onChange = e => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const onSubmit = e => {
+    e.preventDefault();
+    if (password !== passwordConfirm) {
+      console.log('passwords do not match');
+    } else {
+      console.log(formData);
+    }
+  };
+
   return (
-    <div>
-      Register
-    </div>
+    <Fragment>
+      <h1 className="large text-primary">Sign Up</h1>
+      <p className="lead"><i className="fas fa-user"></i> Create Account</p>
+      <form className="form" onSubmit={onSubmit}>
+        <div className="form-group">
+          <input
+            type="text"
+            placeholder="Name"
+            name="name"
+            value={name}
+            onChange={onChange}
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="email"
+            placeholder="Email Address"
+            name="email"
+            value={email}
+            onChange={onChange}
+          />
+          <small className="form-text">
+            This site uses Gravatar so if you want a profile image, use a Gravatar email
+          </small>
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            minLength="6"
+            value={password}
+            onChange={onChange}
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Confirm Password"
+            name="passwordConfirm"
+            minLength="6"
+            value={passwordConfirm}
+            onChange={onChange}
+          />
+        </div>
+        <input type="submit" className="btn btn-black" value="Register" />
+      </form>
+      <p className="my-1">
+        Already have an account? <strong><a href="login.html">Sign In</a></strong>
+      </p>
+    </Fragment>
   )
 };
 

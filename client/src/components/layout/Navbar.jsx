01da136f0fe758f940eed300641b68ed97c7192e
Pawel Dabkowski <paweldabkowski01@gmail.com>
2021-09-13 13:36:09 +0100

    delete stylesheets imports

--- client/src/components/layout/Navbar.jsx
+++ client/src/components/layout/Navbar.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
 
-import '../../assets/stylesheets/components/layout/Navbar.scss';
-
 const Navbar = () => {
   return (
     <nav className="navbar bg-dark">
       <h1>
-        <a href="index.html"><i className="fas fa-code"></i>EngineersWorld</a>
+        <a href="index.html">Engineers World</a>
       </h1>
       <ul>
         <li><a href="profiles.html">Engineers</a></li>

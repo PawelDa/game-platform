ad06e23b81e7197bbc1c4a5f7b82910e491ba001
Pawel Dabkowski <paweldabkowski01@gmail.com>
2021-09-13 13:14:38 +0100

    changes in landing page

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -7,10 +7,9 @@ const Landing = () => {
     <section className="landing">
       <div className="dark-overlay">
         <div className="landing-inner">
-          <h1 className="x-large">Developer Connector</h1>
+          <h1 className="x-large">-Engineers Social Network-</h1>
           <p className="lead">
-            Create a developer profile/portfolio, share posts and get help from
-            other developers
+            Create a profile, share posts and get in touch with other engineers!
           </p>
           <div className="buttons">
             <a href="register.html" className="btn btn-primary">Sign Up</a>

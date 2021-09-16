e9881ae17468181dd20cf389cb6ed69ae878dcf7
Pawel Dabkowski <paweldabkowski01@gmail.com>
2021-09-16 18:24:32 +0100

    profile component to view user details

---
+++ client/src/components/profile/Profile.jsx
@@ -0,0 +1,37 @@
+import React, { Fragment, useEffect } from 'react'
+import { createStructuredSelector } from 'reselect';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+import { getProfileById } from '../../redux/actions/profile';
+import { selectProfile } from '../../redux/selectors/profile';
+import { selectAuth } from '../../redux/selectors/auth';
+
+import Spinner from '../layout/Spinner';
+
+const Profile = ({ match, getProfileById, profile: { profile, loading }, auth }) => {
+  useEffect(() => {
+    getProfileById(match.params.id);
+  }, [getProfileById, match]);
+
+  return (
+    <Fragment>
+      {profile === null || loading ? <Spinner /> : <Fragment>
+        <Link to='/profiles' className='btn btn-white'>
+          Go Back
+        </Link>
+      </Fragment>}
+    </Fragment>
+  )
+};
+
+const mapStateToProps = createStructuredSelector({
+  profile: selectProfile,
+  auth: selectAuth
+});
+
+const mapDispatchToProps = dispatch => ({
+  getProfileById: (userId) => dispatch(getProfileById(userId))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Profile);

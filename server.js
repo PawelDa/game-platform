8d9e9f1de26202d9857115cecd8b145d2436f55b
Pawel Dabkowski <paweldabkowski01@gmail.com>
2021-09-17 18:12:25 +0100

    preparing to deploy

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDb = require('./config/db');
+const path = require('path');
 
 const app = express();
 
@@ -8,15 +9,21 @@ connectDb();
 // Initialize Middleware
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('API running');
-});
-
 // Define Routes
 app.use('/users', require('./routes/users'));
 app.use('/auth', require('./routes/auth'));
 app.use('/profile', require('./routes/profile'));
 app.use('/posts', require('./routes/posts'));
+
+// Serve static assets in production
+if(process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) =>{
+    res.sendFile(path.resolve(__dirname.anchor, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 

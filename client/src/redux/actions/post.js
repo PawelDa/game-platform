2648c2708dcf05051b0c44b64b40f466ad0bafa7
Pawel Dabkowski <paweldabkowski01@gmail.com>
2021-09-17 13:37:36 +0100

    add and remove posts like actions

--- client/src/redux/actions/post.js
+++ client/src/redux/actions/post.js
@@ -3,7 +3,8 @@ import axios from 'axios';
 import { setAlert } from './alert';
 import {
   GET_POSTS,
-  POST_ERROR
+  POST_ERROR,
+  UPDATE_LIKES
 } from './types';
 
 // Get posts
@@ -14,6 +15,40 @@ export const getPosts = () => async dispatch => {
     dispatch({
       type: GET_POSTS,
       payload: res.data
+    });
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};
+
+// Add like
+export const addLike = postId => async dispatch => {
+  try {
+    const res = await axios.put(`/posts/like/${postId}`);
+
+    dispatch({
+      type: UPDATE_LIKES,
+      payload: { postId, likes: res.data }
+    });
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};
+
+// Remove like
+export const removeLike = postId => async dispatch => {
+  try {
+    const res = await axios.put(`/posts/unlike/${postId}`);
+
+    dispatch({
+      type: UPDATE_LIKES,
+      payload: { postId, likes: res.data }
     });
   } catch (err) {
     dispatch({

abb4f3613250b0d30405fa0ea392979ea5fcc264
Pawel Dabkowski <paweldabkowski01@gmail.com>
2021-09-17 13:32:35 +0100

    PostItem to display users post on posts page

--- client/src/components/posts/Posts.jsx
+++ client/src/components/posts/Posts.jsx
@@ -6,17 +6,22 @@ import { getPosts } from '../../redux/actions/post';
 import { selectPosts, selectLoading } from '../../redux/selectors/post';
 
 import Spinner from '../layout/Spinner';
+import PostItem from './PostItem';
 
 const Posts = ({ getPosts, posts, loading }) => {
   useEffect(() => {
     getPosts();
   }, [getPosts]);
 
-  return (
-    <Fragment>
-      POSTSTSTASDSFAF
-    </Fragment>
-  );
+  return loading ? <Spinner /> : <Fragment>
+    <h1 className='large text-primary'>Posts</h1>
+    {/*TODO PostForm*/}
+    <div className='posts'>
+      {posts.map(post => (
+        <PostItem key={post._id} post={post} />
+      ))}
+    </div>
+  </Fragment>;
 };
 
 const mapStateToProps = createStructuredSelector({

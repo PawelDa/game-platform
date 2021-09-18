20bf04783def77a776fd7949881622a9795420c5
Pawel Dabkowski <paweldabkowski01@gmail.com>
2021-09-18 14:48:11 +0100

    deploy prep

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,15 +1,10 @@
 import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
-import logger from 'redux-logger';
 
 import rootReducer from './reducers/root-reducer';
 
 const middlewares = [thunk];
-
-if (process.env.NODE_ENV === 'development') {
-  middlewares.push(logger);
-} 
 
 const store = createStore(
   rootReducer,
--- server.js
+++ server.js
@@ -16,12 +16,17 @@ app.use('/profile', require('./routes/profile'));
 app.use('/posts', require('./routes/posts'));
 
 // Serve static assets in production
+__dirname = path.resolve();
+
 if(process.env.NODE_ENV === 'production') {
-  // Set static folder
-  app.use(express.static('client/build'));
+  app.use(express.static(path.join(__dirname, '/client/build')));
 
   app.get('*', (req, res) =>{
-    res.sendFile(path.resolve(__dirname.anchor, 'client', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+} else {
+  app.get('/', (req, res) => {
+    res.send('API running');
   });
 }
 
